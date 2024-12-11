fa4e90b39c486e42ba41392e2c4c751e0c98bebf
Shubham Gaurav <shubhamgaurav@Shubhams-MacBook-Air.local>
2024-12-11 19:33:38 +0530

    day 2 components

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const heading = React.createElement("h1",{},"Hello World from react");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const jsxHeading = (<h1 id = "heading">Hello world using jsx</h1>);
+console.log(jsxHeading);
+
+
+const HeadingComponent =()=>{
+    return<h1> Namaste React Functional Component </h1>
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root")); 
+
+root.render(jsxHeading);
